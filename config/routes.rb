d384c91636d18737b8282c8bc07aa6f451a96da8
Frank <frank.junheng@gmail.com>
2014-05-22 13:03:48 -0400

    Added controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 DukeVolunteer::Application.routes.draw do
-  get "static_pages/volunteer"
-  get "static_pages/about"
+  get "volunteers/new"
+  root 'static_pages#volunteer'
+  match '/about', to: 'static_pages#about', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
