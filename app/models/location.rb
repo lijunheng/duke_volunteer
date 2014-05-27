a2fe74a2a80e34b35c98019942203fffeac734e8
Frank <frank.junheng@gmail.com>
2014-05-27 14:53:15 -0400

    Table

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,7 @@
 class Location < ActiveRecord::Base
 	belongs_to :organization
+	validates :latitude, presence: true
+	validates :longitude, presence: true
 	geocoded_by :address
-	after_validation :geocode
+	before_validation :geocode
 end
